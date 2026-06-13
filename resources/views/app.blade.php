b8a8f33556943ab9b96f72a07f3bf8b6fa6ed72e
synteravin <ridhwananang@gmail.com>
2026-06-13 21:00:19 +0700

    commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,7 +40,7 @@
         <x-inertia::head>
             <title>Ridhwan Anang Ma'ruf - Fullstack Web Developer (Laravel & React)</title>
             <link rel="canonical" href="https://ridhwananang.id/">
-            <meta name="description" content="Portfolio resmi Ridhwan Anang Ma'ruf, Fullstack Web Developer yang berdedikasi membangun sistem backend Laravel yang andal dan antarmuka React + TypeScript yang interaktif.">
+            <meta name="description" content="Portfolio resmi Ridhwan Anang Ma'ruf, Fullstack Developer spesialis Laravel & React. Membangun aplikasi web skala produksi yang andal.">
             <meta name="keywords" content="Ridhwan Anang Ma'ruf, Ridhwan Anang, Portfolio, Fullstack Web Developer, Laravel Developer, React Developer, Indonesia, Tangerang Selatan">
             <meta name="author" content="Ridhwan Anang Ma'ruf">
 
